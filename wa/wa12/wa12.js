ac8d8ce955ddd77e044425f3a308b568aab1752b
emmamostello <emmo8113@colorado.edu>
2023-11-19 19:45:21 -0700

    wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,14 +1,27 @@
 function getDog() {
-
+    const loadingContainer = document.getElementById('loadingContainer');
+    const loadingBar = document.getElementById('loadingBar');
+    const dogImg = document.getElementById('dogImg');
+    const tweetButton = document.getElementById('tweet');
+  
+   
+    loadingContainer.style.display = 'block';
+  
     fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
-        .then(data => {
-            const dogImg = document.getElementById('dogImg');
-            dogImg.src = data.message;
-        })
-        
-        .catch(error => {
-            console.log('Error fetching dog iage:',error);
-        });
-}
-
+      .then(response => response.json())
+      .then(data => {
+        loadingContainer.style.display = 'none';
+        dogImg.src = data.message;
+        tweetButton.disabled = false;
+      })
+      .catch(error => {
+        console.log('Error fetching dog image:', error);
+        loadingContainer.style.display = 'none';
+      });
+  }
+  
+  function tweetDog() {
+    const dogImg = document.getElementById('dogImg').src;
+    const twitterIntentUrl = `https://twitter.com/intent/tweet?text=Check%20out%20this%20cute%20dog!&url=${encodeURIComponent(dogImg)}`;
+    window.open(twitterIntentUrl, '_blank');
+  }
