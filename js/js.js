9623995fe4b6a6d76f5f7c4be8abb1c304ab58f6
emmamostello <emmo8113@colorado.edu>
2023-12-14 12:33:42 -0700

    Update to JS

--- js/js.js
+++ js/js.js
@@ -1,17 +1,33 @@
 function generateRandomNumber() {
-    // Generate a random 10-digit number
     var randomNum = Math.floor(Math.random() * 9000000000) + 1000000000;
-    
-    // Display the number on the page
-    document.getElementById('randomNumber').innerText = "Is Your Number...: " + randomNum;
-  }
+    document.getElementById('randomNumber').innerText = "Is your number... " + randomNum;
+  
 
+  var formattedNum = '(' + randomNum.toString().substring(0, 3) + ')' +
+                         randomNum.toString().substring(3, 6) + '-' +
+                         randomNum.toString().substring(6);
+
+      document.getElementById('randomNumber').innerText = "Is your number... " + formattedNum;
+}
   function generateNewNumber(response) {
     if (response === 'Yes') {
-      // Move the "Yes" button to a random position
-      moveButton();
+      //moves the "Yes" button to a random position
+      moveButton('yes');
     } else if (response === 'No') {
-      // If the response is "No," generate a new random number
+      //if the response is "No," generate a new random number
       generateRandomNumber();
     }
+  }
+//moving the button around screen 
+  function moveButton(buttonId) {
+    var button = document.getElementById(buttonId + 'Button');
+    var maxX = window.innerWidth - button.clientWidth;
+    var maxY = window.innerHeight - button.clientHeight;
+
+    var newX = Math.floor(Math.random() * maxX);
+    var newY = Math.floor(Math.random() * maxY);
+
+    button.style.position = 'absolute';
+    button.style.left = newX + 'px';
+    button.style.top = newY + 'px';
   }
